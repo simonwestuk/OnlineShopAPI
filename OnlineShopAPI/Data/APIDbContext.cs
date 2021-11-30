65f906811e08c65948c76d14e5372504529b99df
wests <wests@C-010053>
2021-11-30 13:44:43 +0000

    Add project files.

---
+++ OnlineShopAPI/Data/APIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShopAPI.Models;
+
+namespace OnlineShopAPI.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public DbSet<ProductModel> Products { get; set; }
+        public APIDbContext(DbContextOptions ops) : base(ops)
+        {
+
+        }
+    }
+}
---
+++ OnlineShopAPI/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopAPI.Models
+{
+    public class ProductModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+
+    }
+}
