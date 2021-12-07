c7afec164a4310368477fe39bdb69afd499f42f3
wests <wests@C-010053>
2021-12-07 13:28:18 +0000

    Added API

--- OnlineShopAPI/Startup.cs
+++ OnlineShopAPI/Startup.cs
@@ -43,7 +43,7 @@ namespace OnlineShopAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
